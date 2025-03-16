fd3747802bf42efc892a4e15a873aa1fe8cd768c
vincentcollet <vincent.collet@hotmail.com>
2025-03-16 14:23:19 +0100

    Update google_map.js

--- js/google_map.js
+++ js/google_map.js
@@ -3,15 +3,12 @@ document.addEventListener("DOMContentLoaded", function () {
     if (mapContainer) {
         mapContainer.innerHTML = `
             <div class="embed-map-responsive">
-                <div class="embed-map-container">
-                    <iframe class="embed-map-frame"
-                        frameborder="0" 
-                        scrolling="no" 
-                        marginheight="0" 
-                        marginwidth="0" 
-                        src="https://maps.google.com/maps?hl=en&q=La%20Carri%C3%A8re%20Bioul&t=&z=11&ie=UTF8&iwloc=B&output=embed">
-                    </iframe>
-                </div>
+                <iframe 
+                    src="https://maps.google.com/maps?hl=en&q=La%20Carri%C3%A8re%20Bioul&t=&z=11&ie=UTF8&iwloc=B&output=embed"
+                    frameborder="0" 
+                    allowfullscreen
+                    loading="lazy">
+                </iframe>
             </div>
         `;
     }

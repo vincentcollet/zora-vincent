f80c6d15c05db6e2eb0e916fec92f8094952d2b1
Vincent Collet <vincent.collet@amoobi.com>
2025-04-28 00:25:09 +0200

    clean of code and gallerie refonte

--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -1,22 +1,29 @@
 $(document).ready(function() {
-	// MagnificPopup pour toutes les images de la galerie
-	var magnifPopup = function() {
-	  $('#fh5co-gallery-list').magnificPopup({
-		delegate: 'a.image-popup', // seulement les <a> avec class "image-popup"
-		type: 'image',
-		gallery: {
-		  enabled: true // Active la navigation entre les images
-		},
-		zoom: {
-		  enabled: true,
-		  duration: 300,
-		  easing: 'ease-in-out',
-		  opener: function(openerElement) {
-			return openerElement.is('img') ? openerElement : openerElement.find('img');
-		  }
-		}
-	  });
-	};
+    // MagnificPopup pour images et galeries
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-with-zoom',
+        gallery: {
+            enabled: true,
+            delegate: 'a', // <-- important pour les sous-liens
+        },
+        zoom: {
+            enabled: true,
+            duration: 300,
+            easing: 'ease-in-out',
+            opener: function(openerElement) {
+                return openerElement.is('img') ? openerElement : openerElement.find('img');
+            }
+        }
+    });
+
+    $('.gallery-trigger').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+    });
   
 	// MagnificPopup pour les vidéos (YouTube, Vimeo, Google Maps)
 	var magnifVideo = function() {

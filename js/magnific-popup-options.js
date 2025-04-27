cf36bc40b5721c441fc5aa5897f011ebe5714c1d
Vincent Collet <vincent.collet@amoobi.com>
2025-04-28 00:27:51 +0200

    clean of code and gallerie refonte

--- js/magnific-popup-options.js
+++ js/magnific-popup-options.js
@@ -1,44 +1,48 @@
 $(document).ready(function() {
-    // MagnificPopup pour images et galeries
-    $('.image-popup').magnificPopup({
-        type: 'image',
-        removalDelay: 300,
-        mainClass: 'mfp-with-zoom',
-        gallery: {
-            enabled: true,
-            delegate: 'a', // <-- important pour les sous-liens
-        },
-        zoom: {
-            enabled: true,
-            duration: 300,
-            easing: 'ease-in-out',
-            opener: function(openerElement) {
-                return openerElement.is('img') ? openerElement : openerElement.find('img');
-            }
-        }
-    });
+  // MagnificPopup
+	var magnifPopup = function() {
+		$('.image-popup').magnificPopup({
+			type: 'image',
+			removalDelay: 300,
+			mainClass: 'mfp-with-zoom',
+			gallery:{
+				enabled:true
+			},
+			zoom: {
+				enabled: true, // By default it's false, so don't forget to enable it
 
-    $('.gallery-trigger').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-    });
-  
-	// MagnificPopup pour les vidéos (YouTube, Vimeo, Google Maps)
-	var magnifVideo = function() {
-	  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 160,
-		preloader: false,
-		fixedContentPos: false
-	  });
+				duration: 300, // duration of the effect, in milliseconds
+				easing: 'ease-in-out', // CSS transition easing function
+
+				// The "opener" function should return the element from which popup will be zoomed in
+				// and to which popup will be scaled down
+				// By defailt it looks for an image tag:
+				opener: function(openerElement) {
+				// openerElement is the element on which popup was initialized, in this case its <a> tag
+				// you don't need to add "opener" option if this code matches your needs, it's defailt one.
+				return openerElement.is('img') ? openerElement : openerElement.find('img');
+				}
+			}
+		});
 	};
-  
-	// Appeler les fonctions
+
+	var magnifVideo = function() {
+		$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+	};
+
+	
+
+
+	// Call the functions 
 	magnifPopup();
 	magnifVideo();
-  });
-  
+
+});
